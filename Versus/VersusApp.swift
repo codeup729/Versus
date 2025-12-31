c548ba3bb6be3005aa5c74013c67a6b841ccf513
anitejsrivastava <anitejsri22@gmail.com>
2025-12-31 03:22:32 -0500

    Initial Commit

---
+++ Versus/VersusApp.swift
@@ -0,0 +1,17 @@
+//
+//  VersusApp.swift
+//  Versus
+//
+//  Created by Anitej Srivastava on 31/12/25.
+//
+
+import SwiftUI
+
+@main
+struct VersusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
